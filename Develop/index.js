78bee898501cafef3a0025dbac2a7c7aaa667334
Maddie <maddieeckstrom@gmail.com>
2023-10-06 06:30:18 -1000

    added questions to be asked for README creation

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project title?",
+    "Please provide a description of your application.",
+    "What are the installation instructions?",
+    "What is the usage information?",
+    "Please write your contributions here.",
+    "What are the test instructions?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
